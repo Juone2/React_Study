1e04adeaa69635f34a6c6396a27d319b99ff7478
kimjuwon <juwon2298@naver.com>
2023-06-16 12:15:14 +0900

    ✍ :: (#9) State 대신 ref로 값 읽어오기

--- React_mini_project/src/components/UserInput.jsx
+++ React_mini_project/src/components/UserInput.jsx
@@ -1,40 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import classes from "./User.module.css";
 import Card from "./UI/Card";
 import Modal from "./UI/Modal";
 
+/* useRef로 값을 읽어오는 걸 바꿔서 코드 양을 줄일 수 있음 */
+/* 값만 읽고 싶고 아무 것도 바꿀 계획이 없다면 state는 필요 없음 -> ref */
+
 function UserInput({ onAddUser }) {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState("");
   const [error, setError] = useState();
-
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  };
-
-  const handleAgeChange = (e) => {
-    setAge(e.target.value);
-  };
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
 
   const onSubmit = (e) => {
     e.preventDefault();
-    if (name.trim().length === 0 || age.trim().length === 0) {
+
+    console.log(nameInputRef); // current always object ( prop )
+
+    const enteredUserName = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+
+    if (
+      enteredUserName.trim().length === 0 ||
+      enteredUserAge.trim().length === 0
+    ) {
       setError({
         title: "Ivalid age",
         message: "Please enter a valid age > 0 ",
       });
       return;
     }
-    if (+age < 1) {
+    if (+enteredUserAge < 1) {
       setError({
         title: "Ivalid input",
         message: "Please enter a valid name and age",
       });
       return;
     }
-    onAddUser(name, age);
-    setName("");
-    setAge("");
+    onAddUser(enteredUserName, enteredUserAge);
+    nameInputRef.current.value = ""; // ref
+    ageInputRef.current.value = ""; // 초기화
   };
 
   const handleError = () => {
@@ -56,17 +60,15 @@ function UserInput({ onAddUser }) {
           <input
             type="text"
             id="username"
-            value={name}
             placeholder="이름을 입력 해 주세요"
-            onChange={handleNameChange}
+            ref={nameInputRef}
           />
           <label htmlFor="age">Age (Years)</label>
           <input
             type="number"
             id="age"
-            value={age}
             placeholder="나이를 입력 해 주세요"
-            onChange={handleAgeChange}
+            ref={ageInputRef}
           />
           <div>
             <button type="submit">Add users</button>
